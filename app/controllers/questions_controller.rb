8fc0b8d67d1621939f2e55e3ef6df3468e88af4f
Mike Oleynik <oleynik.mike@yandex.ru>
2016-07-05 17:12:13 +0300

    test for index, show, new, edit, create

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,32 @@
 class QuestionsController < ApplicationController
+  before_action :load_question, only: [:show, :edit]
+  
   def index
     @questions = Question.all
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def new
+    @question = Question.new 
+  end
+
+  def edit    
+  end
+
+  def create
+    @question = Question.create(question_params)
+    redirect_to @question
+  end
+
+  private
+  def load_question
+    @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:title, :body)
   end
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 describe QuestionsController do
+  let(:question) { create(:question) }
+  
   describe 'GET #index' do
     let(:questions) { create_list(:question, 2) }
-    before do
-      get :index
-    end
+    
+    before { get :index }
     
     it 'populates an array of all questions' do 
       expect(assigns(:questions)).to match_array(questions)
@@ -14,6 +15,53 @@ describe QuestionsController do
     it 'renders index view' do
       expect(response).to render_template :index
     end
+  end
 
+  describe 'GET #show' do
+    before { get :show, id: question }
+      
+    it 'assigns the requested question to @question' do 
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'renders index view' do
+      expect(response).to render_template :show
+    end
+  end
+
+  describe 'GET #new' do
+    before { get :new }
+      
+    it 'assigns a new Question to @question' do 
+      expect(assigns(:question)).to be_a_new(Question)
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'GET #edit' do
+    before { get :edit, id: question }
+      
+    it 'assigns the requested question to @question' do 
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'renders edit view' do
+      expect(response).to render_template :edit
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves the new question in the database' do
+        expect { post :create, question: attributes_for(:question) }.to change(Question, :count).by(1)
+      end   
+
+      it 'renders create view' do
+        expect(response).to redirect_to question_path(assigns(:question))
+      end
+    end
   end
 end
