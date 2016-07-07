3f9bb2e0a85851c5cae1578d4b8357126452faaa
Mike Oleynik <oleynik.mike@yandex.ru>
2016-07-07 11:45:27 +0300

    sign in story

--- spec/acceptance/sign_in_spec.rb
+++ spec/acceptance/sign_in_spec.rb
@@ -6,26 +6,26 @@ feature 'User sign in', %q{
   I want to be able to sign in
 } do 
 
-  scenario 'Registered user try to sihn in' do
+  scenario 'Registered user try to sign in' do
     User.create!(email: 'user@test.com', password: '12345678')
 
     visit new_user_session_path
     fill_in 'Email', with: 'user@test.com'
     fill_in 'Password', with: '12345678'
-    click_on 'Sign in'
+    click_on 'Log in'
 
     expect(page).to have_content 'Signed in successfully.'
     expect(current_path).to eq root_path
   end
 
-  scenario 'Non-registered user try to sihn in' do
+  scenario 'Non-registered user try to sign in' do
 
     visit new_user_session_path
     fill_in 'Email', with: 'noreply@test.com'
     fill_in 'Password', with: '12345678'
-    click_on 'Sign in'
+    click_on 'Log in'
 
-    expect(page).to have_content 'Invalid email or password.'
+    expect(page).to have_content 'QnaLink 1Link 2Link 3Invalid Email or password.×Invalid Email or password.Log in Email Password Remember me Sign up Forgot your password? SidebarSidebarLink 1Link 2Link 3© Company 2016'
     expect(current_path).to eq new_user_session_path
   end
 
