50abbd01c0afb956d3b874923912b18ffd8d11f7
Mike Oleynik <oleynik.mike@yandex.ru>
2016-08-05 11:46:45 +0300

    form to answer

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -33,6 +33,10 @@ describe QuestionsController do
     it 'renders index view' do
       expect(response).to render_template :show
     end
+
+    it 'builds new attachments for answer' do
+      expect(assigns(:answer).attachments.first).to be_a_new(Attachment)
+    end
   end
 
   describe 'GET #new' do
