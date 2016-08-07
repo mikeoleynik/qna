13fa8f1c27c0140344d106b1ff74e948f9e7bdb9
Mike Oleynik <oleynik.mike@yandex.ru>
2016-08-07 12:31:17 +0300

    add multiple files to question

--- spec/acceptance/add_file_to_question_spec.rb
+++ spec/acceptance/add_file_to_question_spec.rb
@@ -13,13 +13,17 @@ feature 'Add files to question', %q{
     visit new_question_path
   end
 
-  scenario 'User adds file when asks question' do
+  scenario 'User adds file when asks question', js: true do
     fill_in 'Заголовок', with: 'Test question'
     fill_in 'Вопрос', with: 'text'
-    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
-    click_on 'Создать'
+    click_on "Добавить"
+    inputs = all('input[type="file"]')
+    inputs[0].set("#{Rails.root}/spec/spec_helper.rb")
+    inputs[1].set("#{Rails.root}/spec/rails_helper.rb")
+    click_on "Создать"
 
     expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+    expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
   end
 
 end
