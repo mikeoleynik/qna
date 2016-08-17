f987b1d9d3a061c349470a825795e973bda10494
Mike Oleynik <oleynik.mike@yandex.ru>
2016-08-17 14:59:55 +0300

    fix 2

--- app/models/concerns/votable.rb
+++ app/models/concerns/votable.rb
@@ -1,11 +1,15 @@
 module Votable
   extend ActiveSupport::Concern
 
-    included do
-      has_many :votes, as: :votable
-    end
-
     def count_votes
       votes.sum(:rating)
+    end
+
+    def vote_it(rating, user)
+      votes.create(rating: rating, user: user)
+    end
+
+    def vote_empty?(user)
+      Vote.exists?(user: user)
     end
 end
