83fc79fff8ea9e18aaeb9ada8c0b83eeb97335a3
Mike Oleynik <oleynik.mike@yandex.ru>
2016-08-27 10:25:02 +0300

    .find_for_oauth/creates authorization for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,11 @@ class User < ActiveRecord::Base
 
   def self.find_for_oauth(auth)
     authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
-    authorization.user if authorization
+    return authorization.user if authorization
+
+    email = auth.info[:email]
+    user = User.where(email: email).first
+    user.authorizations.create(provider: auth.provider, uid: auth.uid) if user
   end
 
 end
