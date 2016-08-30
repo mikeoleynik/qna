9f4282b75947f8572e73c16a6601c0b16200dd9b
Mike Oleynik <oleynik.mike@yandex.ru>
2016-08-30 09:45:18 +0300

    all spec

--- spec/acceptance/sign_in_with_twitter_spec.rb
+++ spec/acceptance/sign_in_with_twitter_spec.rb
@@ -1,16 +1,21 @@
 require_relative 'acceptance_helper'
 
-feature 'Sign in with Twitter'do
+feature 'Sign in with Twitter' do
   before { visit new_user_session_path }
 
   scenario 'user can sign in with his Twitter account' do
-    mock_auth_hash(:twitter)
-    click_on 'Sign in with Twitter'
+    expect(page).to have_content('Sign in with Twitter')
 
-    fill_in 'auth_info_email', with: 'user@twitter.com'
+    click_on 'Sign in with Twitter'
+    mock_auth_hash(:twitter)
+    OmniAuth.config.add_mock(:twitter, { info: { email: nil } })  
+
+    expect(page).to have_content 'Пожалуйста введите свой e-mail'
+    fill_in 'email', with: 'user_example@example.com'
     click_on 'Send'
 
-    expect(page).to have_content 'Successfully authenticated from Twitter account.'
+    expect(page).to have_content I18n.t('devise.omniauth_callbacks.success', kind: 'Twitter')
+    expect(current_path).to eq root_path
   end
 
   scenario 'authentication error' do
