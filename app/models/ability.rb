a181a357abb004cdaa1c9e60d54a20ac40739784
Mike Oleynik <oleynik.mike@yandex.ru>
2016-08-30 14:06:27 +0300

    ability for quest, admin, user

---
+++ app/models/ability.rb
@@ -0,0 +1,17 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    if user
+      if user.admin?
+        can :manage, :all
+      else
+        can :read, :all
+        can :create, [Question, Answer, Comment]
+        can :update, [Question, Answer, Comment], user: user
+      end
+    else
+      can :read, :all
+    end
+  end
+end
---
+++ spec/models/ability_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe Ability do
+  subject(:ability) { Ability.new(user) }
+
+  describe 'for quest' do
+    let(:user) { nil }
+
+    it {should be_able_to :read, Question }
+    it {should be_able_to :read, Answer }
+    it {should be_able_to :read, Comment }
+
+    it {should_not be_able_to :manage, :all }
+  end
+
+  describe 'for admin' do
+    let(:user) { create :user, admin: true }
+
+    it {should be_able_to :manage, :all }
+  end
+
+  describe 'for user' do
+    let(:user) { create :user }
+    let(:other) { create :user }
+    let(:question) { create :question }
+
+    it {should_not be_able_to :manage, :all }
+    it {should be_able_to :read, :all }
+
+    it {should be_able_to :create, Question }
+    it {should be_able_to :create, Answer }
+    it {should be_able_to :create, Comment }
+
+    it {should be_able_to :update, create(:question, user: user), user: user }
+    it {should_not be_able_to :update, create(:question, user: other), user: user }
+
+    it {should be_able_to :update, create(:answer, question: question, user: user), user: user }
+    it {should_not be_able_to :update, create(:answer, question: question, user: other), user: user }
+
+    it {should be_able_to :update, create(:comment, user: user), user: user }
+    it {should_not be_able_to :update, create(:comment, user: other), user: user }
+  end
+end
