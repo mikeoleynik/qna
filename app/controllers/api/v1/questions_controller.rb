bcbebe748b612f8aa388ba542493f57c4a8a40bd
Mike Oleynik <oleynik.mike@yandex.ru>
2016-09-05 11:21:36 +0300

    questions/index

---
+++ app/controllers/api/v1/questions_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::QuestionsController < Api::V1::BaseController
+
+  authorize_resource class: Question
+  
+  def index
+    @questions = Question.all
+    respond_with @questions
+  end
+end
