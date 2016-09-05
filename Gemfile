80c3b876ea937aedf1ee1c895a6d984571d094e3
Mike Oleynik <oleynik.mike@yandex.ru>
2016-09-05 13:56:36 +0300

    fix

--- Gemfile
+++ Gemfile
@@ -37,6 +37,9 @@ gem 'omniauth-twitter'
 gem 'letter_opener'
 gem 'cancancan'
 gem 'doorkeeper'
+gem 'active_model_serializers'
+gem 'oj'
+gem 'oj_mimic_json'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -20,7 +20,7 @@ describe 'Question API' do
       let(:access_token) { create (:access_token) }
       let!(:questions) { create_list(:question, 2, user: user) }
       let(:question) { questions.first }
-      let!(:answer) { create(:answer, question: question) }
+      let!(:answer) { create(:answer, question: question, user: user) }
 
       before { get '/api/v1/questions', format: :json, access_token: access_token.token }
 
@@ -29,23 +29,23 @@ describe 'Question API' do
       end
 
       it 'returns list of questions' do
-        expect(response.body).to have_json_size(2)
+        expect(response.body).to have_json_size(2).at_path("questions")
       end
 
-      %w(id body title).each do |attr|
+      %w(id body title created_at updated_at).each do |attr|
         it 'question object contains #{attr}' do
-          expect(response.body).to be_json_eql(question.send(attr.to_sym).to_json).at_path(0/"#{attr}")
+          expect(response.body).to be_json_eql(question.send(attr.to_sym).to_json).at_path("questions/0/#{attr}")
         end
       end
 
       context 'answers' do    
         it 'included in question object' do
-          expect(response.body).to have_json_size(1).at_path("0/answers")
+          expect(response.body).to have_json_size(1).at_path("questions/0/answers")
         end
 
-        %w(id body).each do |attr|
+        %w(id body created_at updated_at).each do |attr|
           it 'contains #{attr}' do
-            expect(response.body).to be_json_eql(answer.send(attr.to_sym).to_json).at_path(0/answers/0/"#{attr}")
+            expect(response.body).to be_json_eql(answer.send(attr.to_sym).to_json).at_path("questions/0/answers/0/#{attr}")
           end
         end  
       end
