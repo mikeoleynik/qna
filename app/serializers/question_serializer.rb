2dd1d3863687de198b946583f84148212ffa5b50
Mike Oleynik <oleynik.mike@yandex.ru>
2016-09-06 11:36:21 +0300

    add questions/create && show

--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -2,4 +2,6 @@ class QuestionSerializer < ActiveModel::Serializer
   attributes :id, :title, :body, :created_at, :updated_at
 
   has_many :answers
+  has_many :comments
+  has_many :attachments
 end
