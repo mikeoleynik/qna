11c0f47cca8a77681022d9d6bad5dc2bbc90405f
Mike Oleynik <oleynik.mike@yandex.ru>
2016-09-12 15:23:20 +0300

    fix

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,5 +3,5 @@ class Subscription < ActiveRecord::Base
   belongs_to :question
 
   validates :user_id, :question_id, presence: true
-  validates :user_id, uniqueness: { scope: [:user_id, :question_id] }
+  validates :user_id, uniqueness: { scope: [:question_id] }
 end
