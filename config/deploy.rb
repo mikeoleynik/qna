6d96450b98a3ec63bf8a8c60d14687e9ab9920a2
Mike Oleynik <oleynik.mike@yandex.ru>
2016-09-16 09:19:08 +0300

    restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -70,16 +70,16 @@ namespace :private_pub do
     end
   end
 
-  # desc 'Restart private_pub server'
-  # task :restart do
-  #   on roles(:app) do
-  #     within current_path do
-  #       with rails_env: fetch(:rails_env) do
-  #         execute :bundle, "exec thin -C config/private_pub_thin.yml restart"
-  #       end
-  #     end
-  #   end
-  # end
+  desc 'Restart private_pub server'
+  task :restart do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, "exec thin -C config/private_pub_thin.yml restart"
+        end
+      end
+    end
+  end
 end
 
 after 'deploy:restart', 'private_pub:restart'
