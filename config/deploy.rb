ac470010602c996bcc12a075097b68b96d952bd0
Mike Oleynik <oleynik.mike@yandex.ru>
2016-09-16 09:09:24 +0300

    deplo

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :deploy_user, 'deployer'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{ config/database.yml config/private_pub.yml .env }
+set :linked_files, %w{ config/database.yml config/private_pub_thin.yml .env }
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{ bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads }
