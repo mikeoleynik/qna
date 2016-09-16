51dc2bc361d5bc3e2ed2a8b3033432107023048c
Mike Oleynik <oleynik.mike@yandex.ru>
2016-09-16 15:03:56 +0300

    host prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: '37.139.22.176' }
 end
